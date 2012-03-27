a9a464f998e2b8074775e15553a5bcea28b36d21
zijad.maksuti@gmail.com <zijad.maksuti@gmail.com@96d683f9-5a14-a1d8-75b7-d247282e0859>
2012-03-27 19:20:17 +0000

    [uby] -added wikipedia-asl

---
+++ de.tudarmstadt.ukp.uby.integration.wikipedia-asl/src/main/java/de/tudarmstadt/ukp/lmf/transform/wikipedia/WikipediaENTransformer.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.lmf.transform.wikipedia;
+
+import de.tudarmstadt.ukp.lmf.model.core.GlobalInformation;
+import de.tudarmstadt.ukp.lmf.model.core.LexicalResource;
+import de.tudarmstadt.ukp.lmf.transform.DBConfig;
+import de.tudarmstadt.ukp.wikipedia.api.Wikipedia;
+import de.tudarmstadt.ukp.wikipedia.api.exception.WikiApiException;
+
+/**
+ * Converts the English Wikipedia edition to LMF.
+ * @author Yevgen Chebotar
+ * @author Christian M. Meyer
+*/
+public class WikipediaENTransformer extends WikipediaLMFTransformer {
+
+	public WikipediaENTransformer(final DBConfig dbConfig, 
+			final Wikipedia wiki, final String dtd) throws WikiApiException {
+		super(dbConfig, wiki, dtd);
+	}
+
+	@Override
+	protected String getHiddenCategoryName() {
+		return "Hidden categories";
+	}
+
+	@Override
+	protected String getResourceAlias() {
+		return "WikiEN";
+	}
+
+	@Override
+	protected LexicalResource createLexicalResource() {
+		LexicalResource resource = new LexicalResource();
+		GlobalInformation glInformation = new GlobalInformation();
+		glInformation.setLabel("LMF Representation of Wikipedia, wikiapi_en_20090822");
+		resource.setGlobalInformation(glInformation);
+		resource.setName("WikipediaEN");
+		resource.setDtdVersion(dtd_version);
+		return resource;
+	}
+	
+	protected boolean isDiscussionPage(final String pageTitle) {
+		return pageTitle.startsWith("Discussion:");
+	}
+	
+}
