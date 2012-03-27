f86f644d218c15463fd0ad1b66e81c94a64057b1
zijad.maksuti@gmail.com <zijad.maksuti@gmail.com@96d683f9-5a14-a1d8-75b7-d247282e0859>
2012-03-27 00:33:09 +0000

    [uby] -added framenet-gpl

---
+++ de.tudarmstadt.ukp.uby.integration.framenet-gpl/src/main/java/de/tudarmstadt/ukp/lmf/transform/framenet/FNConverter.java
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ ******************************************************************************/
+package de.tudarmstadt.ukp.lmf.transform.framenet;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import de.saar.coli.salsa.reiter.framenet.FrameNet;
+import de.tudarmstadt.ukp.lmf.model.core.GlobalInformation;
+import de.tudarmstadt.ukp.lmf.model.core.LexicalResource;
+import de.tudarmstadt.ukp.lmf.model.core.Lexicon;
+import de.tudarmstadt.ukp.lmf.model.enums.ELanguageIdentifier;
+import de.tudarmstadt.ukp.lmf.model.semantics.SemanticPredicate;
+
+/**
+ * 
+ * Instance of this class converts
+ * <a href="URL#https://framenet.icsi.berkeley.edu/fndrupal/">FrameNet 1.5</a>
+ * to LMF-format
+ * @author Zijad Maksuti, Silvana Hartmann 
+ *
+ */
+public class FNConverter {
+
+	private FrameNet fn; // FrameNet Object
+	private String dtd_version;
+
+	private LexicalResource lexicalResource;
+	
+	private Logger logger = Logger.getLogger(FNConverter.class.getName());
+
+	/**
+	 * Constructs a {@link FNConverter} based on the consumed parameters
+	 * @param frameNet initialized {@link FrameNet} object
+	 * @param lexicalResource initialized object of  {@link LexicalResource}, which will be filled with FrameNet's data 
+	 * @param dtd_version specifies the version of the .dtd which will be written to lexicalResource
+	 */
+	public FNConverter(FrameNet frameNet, LexicalResource lexicalResource, String dtd_version){
+		this.fn = frameNet;
+		this.lexicalResource = lexicalResource;
+		this.dtd_version = dtd_version;
+	}
+
+	/**
+	 * Converts the informations provided by the initialized {@link FrameNet} object to LMF-format. <br> 
+	 * The result of the conversion can be obtained by calling {@link FNConverter#getLexicalResource()}
+	 */
+	public void toLMF() {
+
+		// Setting attributes of LexicalResource
+		lexicalResource.setName("FrameNet");
+		lexicalResource.setDtdVersion(this.dtd_version);
+
+		// *** Setting GlobalInformation *** //
+		GlobalInformation globalInformation = new GlobalInformation();
+		globalInformation.setLabel("LMF representation of FrameNet 1.5");
+		lexicalResource.setGlobalInformation(globalInformation);
+
+		//*** Setting Lexicon (only one since FrameNet is monolingual)***//
+		Lexicon lexicon = new Lexicon();
+		lexicon.setLanguageIdentifier(ELanguageIdentifier. en);
+		lexicon.setId("FN_Lexicon_0");
+		lexicon.setName("FrameNet");
+		LinkedList<Lexicon> lexicons = new LinkedList<Lexicon>();
+		lexicons.add(lexicon);
+		lexicalResource.setLexicons(lexicons);
+
+		// *** Creating SemanticPredicates *** //
+		logger.log(Level.INFO, "Generating SemanticPredicates...");
+		SemanticPredicateGenerator semanticPredicateGenerator = new SemanticPredicateGenerator(fn);
+		List<SemanticPredicate> semanticPredicates = new ArrayList<SemanticPredicate>();
+		semanticPredicates.addAll(semanticPredicateGenerator.getSemanticPredicates());
+		lexicon.setSemanticPredicates(semanticPredicates);
+		logger.log(Level.INFO, "Generating SemanticPredicates done");
+
+		// *** Creating LexicalEntries *** //
+		logger.log(Level.INFO, "Generating LexicalEntries...");
+		LexicalEntryGenerator lexicalEntryGenerator = new LexicalEntryGenerator(fn, semanticPredicateGenerator);
+		lexicon.setLexicalEntries(lexicalEntryGenerator.getLexicalEntries());
+		logger.log(Level.INFO, "Generating LexicalEntries done");
+	}
+
+
+	/**
+	 * Returns the {@link LexicalResource} object, which contains the results of the conversion 
+	 * @return the lexicalResource
+	 */
+	public LexicalResource getLexicalResource() {
+		return lexicalResource;
+	}
+}
---
+++ de.tudarmstadt.ukp.uby.integration.framenet-gpl/src/main/java/de/tudarmstadt/ukp/lmf/transform/framenet/FNUtils.java
@@ -0,0 +1,158 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ ******************************************************************************/
+package de.tudarmstadt.ukp.lmf.transform.framenet;
+
+import de.saar.coli.salsa.reiter.framenet.CoreType;
+import de.saar.coli.salsa.reiter.framenet.FrameNetRelationDirection;
+import de.saar.coli.salsa.reiter.framenet.PartOfSpeech;
+import de.tudarmstadt.ukp.lmf.model.enums.ECoreType;
+import de.tudarmstadt.ukp.lmf.model.enums.EPartOfSpeech;
+import de.tudarmstadt.ukp.lmf.model.enums.EYesNo;
+import de.tudarmstadt.ukp.lmf.model.semantics.PredicateRelation;
+
+/**
+ * This class offers universal methods for {@link FNConverter}
+ * @author Zijad Maksuti, Silvana Hartmann
+ */
+public class FNUtils {
+	
+	/**
+	 * This method removes the XSL-tags that are contained in FrameNet's files 
+	 * @param aString String containing XSL-tags
+	 * @return String without XSL-tags
+	 */
+	public static String filterTags(String aString){
+		
+		aString = aString.replaceAll("&lt;!--", "");
+		aString = aString.replaceAll("--&gt;", "");
+		
+		aString = aString.replaceAll("<!--", "");
+		aString = aString.replaceAll("-->", "");
+		StringBuffer sb = new StringBuffer(aString);
+		int start;
+		int end;
+		while((start = sb.indexOf("&lt;")) > -1 && (end = sb.substring(start).indexOf("&gt;")) > -1)
+			sb.delete(start, start+end+4);
+		
+		while((start = sb.indexOf("<")) > -1 && (end = sb.substring(start).indexOf(">")) > -1)
+			sb.delete(start, start+end+1);
+		
+		// line feeds
+		String result = sb.toString().replaceAll("\n", "\n");
+		result = sb.toString().replaceAll("&#10;", "\n");
+		
+		
+		// quotation marks
+		result = result.replaceAll("&quot;", "\"");
+		return result;
+	}
+	
+	/**
+	 * This method consumes a part of speech, defined by <a href="http://www.cl.uni-heidelberg.de/trac/FrameNetAPI/wiki/0.4.2"> API used for parsing FrameNet's files</a>,
+	 * <br> and returns the associated part of speech defined in Uby-LMF
+	 * @param pos part of speech defined in {@link PartOfSpeech}
+	 * @return Uby's {@link EPartOfSpeech} that is associated with the consumed pos 
+	 */
+	public static EPartOfSpeech getPOS(PartOfSpeech pos){
+		switch (pos){
+			case Adjective : return EPartOfSpeech.adjective;
+			case Adverb : return EPartOfSpeech.adverb;
+			case Conjunction : return EPartOfSpeech.conjunction;
+			case Determiner : return EPartOfSpeech.determiner;
+			case Interjection : return EPartOfSpeech.interjection;
+			case Noun : return EPartOfSpeech.noun;
+			case Preposition : return EPartOfSpeech.preposition;
+			case Pronoun : return EPartOfSpeech.pronoun;
+			case Verb : return EPartOfSpeech.verb;
+			case SCON : return EPartOfSpeech.subordinatingConjunction;
+			default : return null;
+		}
+	}
+	
+	/**
+	 * Consumes a boolean value and returns associated value in {@link EYesNo}
+	 * @param bool a boolean value
+	 * @return {@link EYesNo#yes} if bool is true, {@link EYesNo#no} otherwise 
+	 */
+	public static EYesNo booleanForHumans(boolean bool){
+		if(bool)
+			return EYesNo.yes;
+		else return EYesNo.no;
+	}
+	
+	/**
+	 * Consumes a core type,
+	 * defined in <a href="http://www.cl.uni-heidelberg.de/trac/FrameNetAPI/wiki/0.4.2"> API used for parsing FrameNet's files</a>
+	 * and returns the associated core type defined in Uby-LMF
+	 * @param coreType core type defined in {@link CoreType}
+	 * @return Uby's {@link ECoreType} that is associated with the consumed coreType
+	 */
+	public static ECoreType getCoreType(CoreType coreType){
+		switch(coreType){
+			case Core : return ECoreType.core;
+			case Peripheral : return ECoreType.peripheral;
+			case Core_Unexpressed : return ECoreType.coreUnexpressed;
+			case Extra_Thematic : return ECoreType.extraThematic;
+			default : return null;
+		}
+	}
+	
+	/**
+	 * Consumes a name of frame relation and relation's direction, as
+	 * defined in <a href="http://www.cl.uni-heidelberg.de/trac/FrameNetAPI/wiki/0.4.2"> API used for parsing FrameNet's files</a>
+	 * and returns the associated relation name of Uby's {@link PredicateRelation}
+	 * @param frameNetRelationName FrameNet's relation name
+	 * @param direction the direction of the relation
+	 * @return name of Uby's PredicateRelation, that corresponds to the consumed frameNetRelationName and the direction
+	 * @see {@link Frame}
+	 * @see {@link FrameNetRelationDirection}
+	 */
+	public static String getRelName(String frameNetRelationName, FrameNetRelationDirection direction){
+		if(frameNetRelationName.equals("Inheritance"))
+			if(direction.equals(FrameNetRelationDirection.UP))
+				return "inherits_from";
+			else return "is_inherited_by";
+		if(frameNetRelationName.equals("Subframe"))
+			if(direction.equals(FrameNetRelationDirection.UP))
+				return "subframe_of";
+			else return "has_subframe";
+		if(frameNetRelationName.equals("Using"))
+			if(direction.equals(FrameNetRelationDirection.UP))
+				return "uses";
+			else return "used_by";
+		if(frameNetRelationName.equals("See_also"))
+			if(direction.equals(FrameNetRelationDirection.UP))
+				return "referring";
+				else return "referred by";
+		if(frameNetRelationName.equals("Inchoative_of"))
+			if(direction.equals(FrameNetRelationDirection.UP))
+				return "inchoative";
+			else return "is_inchoative_of";
+		if(frameNetRelationName.equals("Causative_of"))
+			if(direction.equals(FrameNetRelationDirection.UP))
+				return "causative";
+			else return "is_causative_of";
+		if(frameNetRelationName.equals("Precedes"))
+			if(direction.equals(FrameNetRelationDirection.UP))
+				return "is_preceded_by";
+			else return "precedes";
+		if(frameNetRelationName.equals("Perspective_on"))
+			if(direction.equals(FrameNetRelationDirection.UP))
+				return "perspective_on";
+			else return "is_perspectivized_in";
+		if(frameNetRelationName.equals("ReFraming_Mapping"))
+			if(direction.equals(FrameNetRelationDirection.UP))
+				return "source";
+			else return "target";
+		return null;
+	}
+	
+}
