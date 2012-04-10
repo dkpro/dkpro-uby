0f0f61cad3f0802d63dfa02fe313b5c38bbdbb19
reckart <richard.eckart@gmail.com>
2012-04-10 15:08:28 +0000

    - Added license checking plugins checking GPL and ASL - Make sure you copy a .activate_license-check or .activate_rat-check file from an existing module to new ASL or GPL modules. - Cleaned up POMs

--- de.tudarmstadt.ukp.uby.integration.wordnet-gpl/src/main/java/de/tudarmstadt/ukp/lmf/transform/wordnet/RelatedFormGenerator.java
+++ de.tudarmstadt.ukp.uby.integration.wordnet-gpl/src/main/java/de/tudarmstadt/ukp/lmf/transform/wordnet/RelatedFormGenerator.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/**
  * Copyright 2012
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
@@ -7,7 +7,7 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl-3.0.txt
- ******************************************************************************/
+ */
 package de.tudarmstadt.ukp.lmf.transform.wordnet;
 
 
