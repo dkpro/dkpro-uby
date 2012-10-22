73d7130632f44db9ff2fa7919f507341c9d3f1bf
mikkl666 <matu011235@googlemail.com>
2012-10-22 15:18:55 +0000

    Updated POS to CamelCase prefix notation

--- de.tudarmstadt.ukp.uby.lmf.model-asl/src/main/java/de/tudarmstadt/ukp/lmf/model/enums/EPartOfSpeech.java
+++ de.tudarmstadt.ukp.uby.lmf.model-asl/src/main/java/de/tudarmstadt/ukp/lmf/model/enums/EPartOfSpeech.java
@@ -2,13 +2,13 @@
  * Copyright 2012
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,39 +19,65 @@ package de.tudarmstadt.ukp.lmf.model.enums;
 
 /**
  * Enumeration of different part of speech properties
- * assigned to a word based on its grammatical and semantic properties. 
+ * assigned to a word based on its grammatical and semantic properties.
  * @author Zijad
  *
  */
 public enum EPartOfSpeech {
-	
+
+	abbreviation,
+	abbreviationAcronym,
+	abbreviationInitialism,
 	adjective,
 	adverb,
-	pronominalAdverb,
-	preposition,
-	postposition,
-	circumposition,
+	adverbPronominal,
+	adpositionPreposition,
+	adpositionPostposition,
+	adpositionCircumposition,
+	affix,
+	affixPrefix,
+	affixSuffix,
+	contraction,
 	determiner,
-    possessiveDeterminer,
-    indefiniteDeterminer,
+	determinerArticle,
+    determinerPossessive,
+    determinerIndefinite,
+    determinerDemonstrative,
     numeral,
     interjection,
+    interjectionSalutation,
+    interjectionOnomatopoeia,
+    phraseme,
+    phrasemeIdiom,
+    phrasemeCollocation,
+    phrasemeProverb,
+    phrasemeMnemonic,
     conjunction,
-    coordinatingConjunction,
-    subordinatingConjunction,
+    conjunctionCoordinatin,
+    conjunctionSubordinating,
     noun,
     nounCommon,
     nounProper,
+    nounProperFirstName,
+    nounProperLastName,
+    nounProperToponym,
     pronoun,
-    personalPronoun,
-    reflexivePersonalPronoun,
-    irreflexivePersonalPronoun,    
-    interrogativePronoun,
+    pronounPersonal,
+    pronounPossessive,
+    pronounDemonstrative,
+    pronounRelative,
+    pronounIndefinite,
+    ponounPersonalReflexive,
+    ponounPersonalIrreflexive,
+    pronounInterrogative,
     particle,
-    negativeParticle,
-    infinitiveParticle,
-    comparativeParticle,
-    answerParticle,
+    particleNegative,
+    particleInfinitive,
+    particleComparative,
+    particleAnswering,
+    particleModal,
+    particleFocus,
+    particleIntensifying,
     verbAuxiliary,
     verbModal,
     verbMain,
