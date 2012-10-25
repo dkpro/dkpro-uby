46ac7537531f37bea9a3680682d4da99e5676fb5
mikkl666 <matu011235@googlemail.com>
2012-10-25 12:25:37 +0000

    typo corrected

--- de.tudarmstadt.ukp.uby.lmf.model-asl/src/main/java/de/tudarmstadt/ukp/lmf/model/enums/ELabelTypeSemantics.java
+++ de.tudarmstadt.ukp.uby.lmf.model-asl/src/main/java/de/tudarmstadt/ukp/lmf/model/enums/ELabelTypeSemantics.java
@@ -2,13 +2,13 @@
  * Copyright 2012
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,17 +19,17 @@ package de.tudarmstadt.ukp.lmf.model.enums;
 
 /**
  * Label types for {@link SemanticLabels}s
- * 
+ *
  * @author Eckle-Kohler
  *
  */
 public class ELabelTypeSemantics
 {
 
-	
+
 	public static final String CATEGORY = "category";
 	public static final String DOMAIN = "domain";
-	public static final String SYNTAX = "domain";
+	public static final String SYNTAX = "syntax";
 	public static final String TIMEPERIODOFUSAGE = "timePeriodOfUsage";
 	public static final String REGISTER = "register";
 	public static final String REGIONOFUSAGE = "regionOfUsage";
@@ -42,6 +42,6 @@ public class ELabelTypeSemantics
 	public static final String SEMANTICCATEGORY = "semanticCategory";
 	public static final String COLLOCATE = "collocate";
 	public static final String RESOURCESPECIFIC = "resourceSpecific";
-	
+
 
 }
