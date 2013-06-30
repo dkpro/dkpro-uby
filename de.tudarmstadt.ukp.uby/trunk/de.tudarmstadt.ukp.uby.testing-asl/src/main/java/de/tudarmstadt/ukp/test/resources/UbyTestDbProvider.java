c84a817cd6267bc8ee952275e92e0e94fb7b33d5
eckle.kohler@gmail.com <eckle.kohler@gmail.com@96d683f9-5a14-a1d8-75b7-d247282e0859>
2013-06-30 18:45:54 +0000

    Issue 44: moved UbyTest to api module split off UbyTestDbProvider and left this in the testing module

---
+++ de.tudarmstadt.ukp.uby/trunk/de.tudarmstadt.ukp.uby.testing-asl/src/main/java/de/tudarmstadt/ukp/test/resources/UbyTestDbProvider.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.test.resources;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.dom4j.DocumentException;
+
+import de.tudarmstadt.ukp.lmf.api.Uby;
+import de.tudarmstadt.ukp.lmf.exceptions.UbyInvalidArgumentException;
+import de.tudarmstadt.ukp.lmf.transform.DBConfig;
+import de.tudarmstadt.ukp.lmf.transform.LMFDBUtils;
+import de.tudarmstadt.ukp.lmf.transform.XMLToDBTransformer;
+
+/**
+ * @author Eckle-Kohler
+ * Provides an in-memory UBY test DB for testing.
+ *
+ */
+public class UbyTestDbProvider {
+
+	private Uby uby;
+
+	public UbyTestDbProvider() throws UbyInvalidArgumentException, DocumentException, FileNotFoundException{
+		String uby_user = "root";
+		String uby_pass = "pass";
+
+		DBConfig dbConfig = 
+				new DBConfig("not_important","org.h2.Driver","h2",uby_user,uby_pass,true);
+			LMFDBUtils.createTables(dbConfig);
+			XMLToDBTransformer trans = new XMLToDBTransformer(dbConfig);
+			File ubyTestXmlFile = new File("src/main/resources/UbyTestLexicon.xml");
+			trans.transform(ubyTestXmlFile,"UbyTest");
+				
+	}
+
+	public Uby getUby() {
+		return uby;
+	}
+
+
+}
