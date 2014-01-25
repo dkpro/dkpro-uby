d7506e1895812f350506945663b8fa2b97d2df0b
yevgen.chebotar@gmail.com <yevgen.chebotar@gmail.com@96d683f9-5a14-a1d8-75b7-d247282e0859>
2014-01-25 14:59:10 +0000

    Issue 74. Import of alignment classes from UBY non-free.

---
+++ de.tudarmstadt.ukp.uby.integration.alignment-asl/src/main/java/de/tudarmstadt/ukp/lmf/transform/sensealignments/SenseAlignment.java
@@ -0,0 +1,356 @@
+/*******************************************************************************
+ * Copyright 2013
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package de.tudarmstadt.ukp.lmf.transform.sensealignments;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.xml.transform.TransformerException;
+
+import org.dom4j.DocumentException;
+
+import de.tudarmstadt.ukp.lmf.api.Uby;
+import de.tudarmstadt.ukp.lmf.exceptions.UbyInvalidArgumentException;
+import de.tudarmstadt.ukp.lmf.model.core.GlobalInformation;
+import de.tudarmstadt.ukp.lmf.model.core.LexicalResource;
+import de.tudarmstadt.ukp.lmf.model.core.Sense;
+import de.tudarmstadt.ukp.lmf.model.enums.ESenseAxisType;
+import de.tudarmstadt.ukp.lmf.model.meta.MetaData;
+import de.tudarmstadt.ukp.lmf.model.multilingual.SenseAxis;
+import de.tudarmstadt.ukp.lmf.transform.DBConfig;
+import de.tudarmstadt.ukp.lmf.transform.XMLToDBTransformer;
+import de.tudarmstadt.ukp.lmf.writer.LMFWriterException;
+import de.tudarmstadt.ukp.lmf.writer.xml.LMFXmlWriter;
+
+abstract public class SenseAlignment
+{
+	protected static String LF = System.getProperty("line.separator");
+	protected static String UBY_HOME=System.getenv("UBY_HOME");
+	private String sourceUrl;
+	private String destUrl;
+
+	private String alignmentFileLocation;
+	private File alignment;
+
+	private final List<Sense> sourcesSenses;
+	private final List<Sense> destinationsSenses;
+
+	protected Uby ubySource, ubyDest;
+	
+	protected HashMap<String, MetaData> metaData;
+
+	protected List<String> metaDataIds;
+	protected List<Double> confidences;
+	
+	public SenseAlignment(String sourceUrl, String destUrl, String alignmentFile)
+	{
+		this.sourceUrl = sourceUrl;
+		this.destUrl = destUrl;
+		this.alignmentFileLocation = alignmentFile;
+		sourcesSenses = new ArrayList<Sense>();
+		destinationsSenses = new ArrayList<Sense>();
+		metaData = new HashMap<String, MetaData>();
+		confidences = new ArrayList<Double>();
+		metaDataIds = new ArrayList<String>();
+	}
+
+
+	public SenseAlignment(String sourceUrl, String destUrl, String dbDriver, String dbVendor, String user, String pass, String UBY_HOME)
+	{
+
+		this.sourceUrl = sourceUrl;
+		this.destUrl = destUrl;
+		this.alignmentFileLocation = null;
+		this.alignment = null;
+
+		sourcesSenses = new ArrayList<Sense>();
+		destinationsSenses = new ArrayList<Sense>();
+		metaData = new HashMap<String, MetaData>();
+		confidences = new ArrayList<Double>();
+		metaDataIds = new ArrayList<String>();
+		
+		DBConfig dbConfigSource = new DBConfig(
+				sourceUrl,dbDriver,dbVendor, user,
+				pass, false);
+		try {
+			ubySource = new Uby(dbConfigSource);
+		} catch (UbyInvalidArgumentException e) {
+			e.printStackTrace();
+		}
+		if (!sourceUrl.equals(destUrl)){
+			DBConfig dbConfigDest = new DBConfig(destUrl,dbDriver,dbVendor, user, pass, false);
+			try {
+				ubyDest=new Uby(dbConfigDest);
+			} catch (UbyInvalidArgumentException e) {
+				e.printStackTrace();
+			}
+		}else{
+			ubyDest = ubySource;
+		}
+	}
+	
+	 public SenseAlignment(String sourceUrl, String destUrl,
+			 String dbDriver, String dbVendor, String alignmentFile, String user, String pass, String UBY_HOME)
+	 {
+
+		this.sourceUrl = sourceUrl;
+		this.destUrl = destUrl;
+		this.alignmentFileLocation = alignmentFile;
+		this.alignment = new File(alignmentFile);
+
+		sourcesSenses = new ArrayList<Sense>();
+		destinationsSenses = new ArrayList<Sense>();
+		metaData = new HashMap<String, MetaData>();
+		confidences = new ArrayList<Double>();
+		metaDataIds = new ArrayList<String>();
+		
+		if (!alignment.exists() && !alignment.isFile()) {
+			System.out.println("Alignment file: " + alignmentFile + " doesn't exist! ");
+			System.exit(1);
+		}
+
+		DBConfig dbConfigSource = new DBConfig(sourceUrl,dbDriver,dbVendor, user, pass, false);
+		try {
+			ubySource = new Uby(dbConfigSource);
+		} catch (UbyInvalidArgumentException e) {
+			e.printStackTrace();
+		}
+		if (!sourceUrl.equals(destUrl)){
+			DBConfig dbConfigDest = new DBConfig(destUrl,dbDriver,dbVendor, user,pass, false);
+			try {
+				ubyDest = new Uby(dbConfigDest);
+			} catch (UbyInvalidArgumentException e) {
+				e.printStackTrace();
+			}
+		}else{
+			ubyDest = ubySource;
+		}
+	}
+
+	public void setSourceUrl(String sourceUrl)
+	{
+		this.sourceUrl = sourceUrl;
+	}
+
+	public void setDestinationUrl(String destUrl)
+	{
+		this.destUrl = destUrl;
+	}
+
+	public void setAlignmentFileLocation(String alignmentFileLocation)
+	{
+		this.alignmentFileLocation = alignmentFileLocation;
+		this.alignment = new File(alignmentFileLocation);
+		if (!alignment.exists() && !alignment.isFile()) {
+			System.out.println("Alignment file: " + alignmentFileLocation
+					+ " doesn't exist! ");
+			System.exit(1);
+		}
+	}
+
+	public String getSourceUrl()
+	{
+		return sourceUrl;
+	}
+
+	public String getDestinationUrl()
+	{
+		return destUrl;
+	}
+
+	public String getAlignmentFileLocation()
+	{
+		return alignmentFileLocation;
+	}
+
+	public File getAlignmentFile(){
+		return alignment;
+	}
+
+	public List<Sense> getSourceSenses(){
+		return sourcesSenses;
+	}
+
+	public List<Sense> getDestSenses(){
+		return destinationsSenses;
+	}
+
+	public void addSourceSense(Sense sense){
+		sourcesSenses.add(sense);
+	}
+
+	public void addDestSense(Sense sense){
+		destinationsSenses.add(sense);
+	}
+
+	/**
+	 * depending on each alignment file format.
+	 * @throws UbyInvalidArgumentException
+	 */
+	abstract public void getAlignment() throws UbyInvalidArgumentException;
+
+	
+	/**
+	 * Adds MetaData id and confidence to the current alignment
+	 * If one of attributes is not available set it to null.
+	 * @param metaDataId
+	 * @param confidence
+	 */
+	protected void addMetaData(String metaDataId, double confidence){
+		metaDataIds.add(metaDataId);
+		confidences.add(confidence);
+	}
+	
+	/**
+	 * Parses MetaData at the beginning of an alignment file.
+	 * Requires blocks with '::MetaData'- and '::Alignments'-headers in the alignment file.
+	 * If '::MetaData'-block is not found, returns BufferedReader pointing at the beginning of the file
+	 * @return BufferedReader pointing at the start of '::Alignments'-block
+	 * @throws IOException 
+	 */
+	protected BufferedReader parseMetaData() throws IOException{
+		
+		BufferedReader reader = new BufferedReader(
+				new FileReader(getAlignmentFileLocation()));
+		
+		String line = reader.readLine();
+		if(line == null || !line.equals("::MetaData")){
+			reader.close();
+			return new BufferedReader(
+					new FileReader(getAlignmentFileLocation()));
+		}
+		
+		while ((line = reader.readLine()) != null) {
+			if(line.equals("::Alignments"))
+				break;
+			
+			String[] lineParts = line.split("#");
+			MetaData meta = new MetaData();
+			if(lineParts.length == 6){
+				String id = lineParts[0];
+				meta.setId(id);
+				meta.setCreationDate(lineParts[1].equals("null")?null:new Date(Long.parseLong(lineParts[1])));
+				meta.setCreationTool(lineParts[2].equals("null")?null:lineParts[2]);
+				meta.setVersion(lineParts[3].equals("null")?null:lineParts[3]);
+				meta.setAutomatic(lineParts[4].equals("null")?null:Boolean.parseBoolean(lineParts[4]));
+				meta.setCreationProcess(lineParts[5].equals("null")?null:lineParts[5]);
+				metaData.put(id, meta);
+			}else{
+				System.err.println("Can't parse MetaData: "+line);
+			}
+		}
+		return reader;
+	}
+	/**
+	 * convert the alignment to LMF format
+	 * @throws LMFWriterException
+	 * @throws TransformerException
+	 * @throws IOException
+	 */
+	public void toLMF(String idPrefix, boolean crosslingual, String dtdVersion,String UBY_HOME) throws LMFWriterException, IOException, TransformerException
+	{
+		toLMF(idPrefix,crosslingual,true,dtdVersion,UBY_HOME);
+	}
+
+	/**
+	 *
+	 * @param idPrefix
+	 * @param crosslingual
+	 * @param usingSynsetAxis
+	 * @throws LMFWriterException
+	 * @throws IOException
+	 * @throws TransformerException
+	 */
+
+	public void toLMF(String idPrefix, boolean crosslingual, boolean usingSynsetAxis,String dtdVersion,String UBY_HOME) throws LMFWriterException, IOException, TransformerException
+	{
+		LMFXmlWriter xmlWriter = new LMFXmlWriter(UBY_HOME+"/target/"+idPrefix+".xml", UBY_HOME+"/resources/dtd/DTD_unifiedModel_"+dtdVersion+".dtd");
+
+		LexicalResource lexicalResource = new LexicalResource();
+		List<SenseAxis> senseAxes=new ArrayList<SenseAxis>();
+
+		boolean metaDataNotEmpty = metaData.size() > 0;
+		if(metaDataNotEmpty){
+			List<MetaData> metaDataList = new ArrayList<MetaData>(metaData.size());
+			int i = 0;
+			for(String metaDataId : metaData.keySet()){
+				MetaData meta = metaData.get(metaDataId);
+				meta.setId(idPrefix+"_Meta_"+i);
+				metaDataList.add(meta);
+				i++;
+			}
+			lexicalResource.setMetaData(metaDataList);
+		}
+		for (int i = 0; i < sourcesSenses.size(); i++) {
+			SenseAxis senseAxis = new SenseAxis();
+			//Set type of alignment
+			if(crosslingual) {
+				senseAxis.setSenseAxisType(ESenseAxisType.crosslingualSenseAlignment);
+			} else {
+				senseAxis.setSenseAxisType(ESenseAxisType.monolingualSenseAlignment);
+			}
+			senseAxis.setSenseOne(sourcesSenses.get(i));
+			senseAxis.setSenseTwo(destinationsSenses.get(i));
+			if (usingSynsetAxis){
+				senseAxis.setSynsetOne(sourcesSenses.get(i).getSynset());
+				senseAxis.setSynsetTwo(destinationsSenses.get(i).getSynset());
+			}
+			senseAxis.setId(idPrefix+"_"+i);
+			
+			
+			if(metaDataNotEmpty){
+				String metaDataId = metaDataIds.get(i);
+				if(metaDataId != null){
+					MetaData meta = metaData.get(metaDataId);
+					senseAxis.setMetaData(meta);
+				}
+				senseAxis.setConfidence(confidences.get(i));
+			}
+			
+			senseAxes.add(senseAxis);
+			System.out.println(senseAxes.size());
+			//save them to database
+			//xmlWriter.writeElement(senseAxis);
+//			uby.getSession().save(senseAxis);
+//			Transaction   transaction = uby.getSession().beginTransaction();
+//			transaction.commit();
+		}
+		lexicalResource.setSenseAxes(senseAxes);
+		lexicalResource.setDtdVersion(dtdVersion);
+		lexicalResource.setName("Uby_Alignments_"+idPrefix);
+		GlobalInformation globalInformation=new GlobalInformation();
+		globalInformation.setLabel("Alignments_"+idPrefix);
+		lexicalResource.setGlobalInformation(globalInformation);
+		xmlWriter.writeElement(lexicalResource);
+		xmlWriter.writeEndDocument();
+
+	}
+
+	public static void toDB(DBConfig dbConfig, File xmlSource, String idPrefix) throws DocumentException, FileNotFoundException, UbyInvalidArgumentException{
+		XMLToDBTransformer xml2DB = new XMLToDBTransformer(dbConfig);
+		xml2DB.transform(xmlSource,"Uby_Alignments_"+idPrefix);
+	}
+}
---
+++ de.tudarmstadt.ukp.uby.integration.alignment-asl/src/main/java/de/tudarmstadt/ukp/lmf/transform/sensealignments/WikiEnDeSenseAlignment.java
@@ -0,0 +1,258 @@
+/*******************************************************************************
+ * Copyright 2013
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package de.tudarmstadt.ukp.lmf.transform.sensealignments;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.List;
+
+import de.tudarmstadt.ukp.lmf.model.core.Sense;
+import de.tudarmstadt.ukp.lmf.transform.DBConfig;
+
+public class WikiEnDeSenseAlignment extends SenseAlignment
+{
+	private StringBuilder logString;
+
+	public WikiEnDeSenseAlignment(String sourceUrl,
+			String destUrl,String dbDriver, String dbVendor,
+			String alignmentFile, String user, String pass, String UBY_HOME) throws FileNotFoundException
+	{
+		super(sourceUrl, destUrl,dbDriver,dbVendor, alignmentFile, user, pass,UBY_HOME);
+		logString = new StringBuilder();
+	}
+	
+	public WikiEnDeSenseAlignment(String sourceUrl,
+			String destUrl,String dbDriver, String dbVendor,
+			String alignmentFile, String user, String pass) throws FileNotFoundException
+	{
+		super(sourceUrl, destUrl,dbDriver,dbVendor, alignmentFile, user, pass,UBY_HOME);
+		logString = new StringBuilder();
+	}
+
+	@Override
+	public void getAlignment()
+	{
+		try {
+			System.out.println(getAlignmentFileLocation());
+			BufferedReader reader = new BufferedReader(new FileReader(getAlignmentFileLocation()));
+			String line = null;
+			int lineNumber = 0;
+			//int count = 0;
+			while ((line = reader.readLine()) != null) {
+				lineNumber++;
+				if (lineNumber != 1) {
+					String[] tmp = line.split("\t");
+					String titleWikiEn = tmp[0];
+					String titleWikiDe = tmp[1];
+					System.out.println("...processing line: "+lineNumber+"...");
+					List<Sense> sensesSource = ubySource.getSensesByOriginalReference("Wikipedia",titleWikiEn);
+					List<Sense> sensesDest = ubyDest.getSensesByOriginalReference("Wikipedia",titleWikiDe);
+
+					Sense source = null, dest = null;
+					if (sensesSource.size() != 0 && sensesDest.size() != 0) {
+						for (Sense senseSource : sensesSource) {
+							if (senseSource.getMonolingualExternalRefs().get(0)
+									.getExternalReference().equals(titleWikiEn)) {
+								source = senseSource;
+								break;
+							}
+						}
+						for (Sense senseDest : sensesDest) {
+							if (senseDest.getMonolingualExternalRefs().get(0)
+									.getExternalReference().equals(titleWikiDe)) {
+								dest = senseDest;
+								break;
+							}
+						}
+
+						if (source != null && dest != null) {
+							addSourceSense(source);
+							addDestSense(dest);
+						}
+						else {
+							// log
+							System.out.println("Log mistakes!" + tmp[0] + " \t" + tmp[1]);
+							logString.append(LF);
+							logString.append(titleWikiEn + "\t" + titleWikiDe);
+						}
+					}
+					else {
+						// log
+						System.out.println("Log mistakes!" + tmp[0] + " \t" + tmp[1]);
+						logString.append(LF);
+						logString.append(titleWikiEn + "\t" + titleWikiDe);
+					}
+				}
+			}
+			reader.close();
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	public void insertAlignment(String sourceUrl,
+			String destUrl,String dbDriver, String dbVendor, String user, String pass,String UBY_HOME) 
+			throws SQLException, ClassNotFoundException, IOException
+	{
+		Class.forName(dbDriver);
+		Connection connection = DriverManager.getConnection("jdbc:" + dbVendor + "://" + sourceUrl,user,pass);
+		Statement statement  = connection.createStatement();
+		//ResultSet rs = statement  .executeQuery("SELECT externalReference, senseId FROM MonolingualExternalRef where senseId like 'WikiD%'");
+		FileReader in = new FileReader(UBY_HOME + "/alignment_wp_en_de_all_titles_keys_2012_02_27.txt");
+
+		BufferedReader input =  new BufferedReader(in);
+		String line;
+		int count = 1;
+		boolean start = true;
+		while((line =input.readLine())!=null){
+			if(start) {
+				start = false;
+				continue;
+			}
+			String[] tokens = line.split("\t");
+			String wpdename = tokens[3];
+			String wpenname = tokens[2];
+			statement.executeUpdate("Insert into SenseAxis(senseAxisId,senseAxisType,senseOneId,senseTwoId,lexicalResourceId,idx) Values('WP_en_de_alignment_"+count+"','crosslingualSenseAlignment','"+wpenname+"','"+wpdename+"','Uby',"+count+")");
+			System.out.println(count++);
+		}
+		input.close();
+	}
+	
+	public void correctAlignmentFile(String sourceUrl,
+			String destUrl,String dbDriver, String dbVendor, String user, String pass,String UBY_HOME) throws SQLException, ClassNotFoundException, IOException
+	{
+		Class.forName(dbDriver);
+		Connection connection = DriverManager.getConnection("jdbc:"+dbVendor+"://"+sourceUrl,user,pass);
+		Statement statement  = connection.createStatement();
+		HashMap<String,String> wpde = new HashMap<String, String>();
+		HashMap<String,String> wpen = new HashMap<String, String>();
+		ResultSet rs = statement  .executeQuery("SELECT externalReference, senseId FROM MonolingualExternalRef where senseId like 'WikiD%'");
+
+		while (rs.next()){
+			wpde.put(rs.getString(1), rs.getString(2));
+		}
+		System.out.println("First filled");
+		rs = statement .executeQuery("SELECT externalReference, senseId FROM MonolingualExternalRef where senseId like 'WikiE%'");
+		while (rs.next()){
+			wpen.put(rs.getString(1), rs.getString(2));
+		}
+		System.out.println("Second filled");
+		FileReader in = new FileReader("/home/matuschek/UBY_HOME/alignment_wp_en_de_all_titles_keys_2011_11_08.txt");
+
+		BufferedReader input =  new BufferedReader(in);
+		String line;
+		FileOutputStream out = new FileOutputStream("target/WPDEEN_aligment");
+
+          // Connect print stream to the output stream
+        PrintStream p = new PrintStream(out);
+		while((line =input.readLine()) != null){
+
+			 String[] tokens = line.split("\t");
+			 String wpdename = tokens[1];
+			 String wpenname = tokens[0];
+			 String wpenid = wpen.get(wpenname);
+			 String wpdeid = wpde.get(wpdename);
+			 p.println(wpenname +"\t"+wpdename +"\t"+wpenid +"\t"+wpdeid);
+			 System.out.println(wpenname +"\t"+wpdename +"\t"+wpenid +"\t"+wpdeid);
+		 }
+		input.close();
+		p.close();
+	}
+	
+	
+	public void getAlignmentQuickly(String sourceUrl,
+			String destUrl,String dbDriver, String dbVendor, String user, String pass,String UBY_HOME){
+		String line = null;
+		try {
+			// new
+			SenseAlignmentUtils saUtils;
+
+			DBConfig s = new DBConfig(sourceUrl,dbDriver,dbVendor,user,pass,true);
+			DBConfig d = new DBConfig(destUrl,dbDriver,dbVendor,user,pass,true);
+
+			// temp_Duc is the name of temporary table
+			if(sourceUrl.equals(destUrl)) {
+				saUtils = new SenseAlignmentUtils(s, s, 0, 0, "temp_Duc3", "temp_Duc3");
+			}
+			else {
+				saUtils = new SenseAlignmentUtils(s, d, 0, 0, "temp_Duc3", "temp_Duc3");
+			}
+			saUtils.createDefaultTempTables(false);
+
+			System.out.println(getAlignmentFileLocation());
+			BufferedReader reader = new BufferedReader(new FileReader(getAlignmentFileLocation()));
+
+			int lineNumber = 0;
+			//int count = 0;
+			//saUtils.getSensesByExternalRefID("Aldege \"Baz\" Bastien Memorial Award", 0, false);
+			while ((line = reader.readLine()) != null) {
+				lineNumber++;
+				line = new String(line.getBytes(),"UTF-8");
+				if (lineNumber != 1) {
+					String[] tmp = line.split("\t");
+					String titleWikiEn = tmp[0];
+					String titleWikiDe = tmp[1];
+					System.out.println("...processing line: "+lineNumber+"...");
+
+					List<Sense> sensesSource = saUtils.getSensesByExternalRefID(titleWikiEn, 0, false);
+					List<Sense> sensesDest = saUtils.getSensesByExternalRefID(titleWikiDe, 1, false);
+
+					Sense source = null, dest = null;
+					if (sensesSource.size() == 1 && sensesDest.size() == 1) {
+						source = sensesSource.get(0);
+						dest = sensesDest.get(0);
+
+						if (source != null && dest != null) {
+							addSourceSense(source);
+							addDestSense(dest);
+						}
+						else {
+							System.out.println("Log mistakes!" + tmp[0] + " \t"+ tmp[1]);
+							logString.append(LF);
+							logString.append(titleWikiEn + "\t" + titleWikiDe);
+						}
+					}
+					else {
+						// log
+						System.out.println("Log mistakes!" + tmp[0] + " \t"+ tmp[1]);
+						logString.append(LF);
+						logString.append(titleWikiEn + "\t" + titleWikiDe);
+					}
+				}
+			}
+			reader.close();
+			//	saUtils.destroyTempTable();
+		}
+		catch (Exception ex) {
+			System.out.println("Error happens here:"+line);
+			ex.printStackTrace();
+		}
+	}
+}
+
