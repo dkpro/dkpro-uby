54c324b2169a2a5a45ba54bf47a4b06fec811420
engg.probir.cse@gmail.com <engg.probir.cse@gmail.com@96d683f9-5a14-a1d8-75b7-d247282e0859>
2014-05-29 06:26:24 +0000

    Issue 27: Filter getSensesByOriginalReference() by lexicon

--- de.tudarmstadt.ukp.uby.testing-asl/src/test/java/de/tudarmstadt/ukp/uby/lmf/api/test/UbyTest.java
+++ de.tudarmstadt.ukp.uby.testing-asl/src/test/java/de/tudarmstadt/ukp/uby/lmf/api/test/UbyTest.java
@@ -55,21 +55,18 @@ import de.tudarmstadt.ukp.test.resources.UbyTestDbProvider;
  */
 public class UbyTest
 {
-	
-	private Uby uby;
-	
+
+	private final Uby uby;
+
 	public UbyTest() throws FileNotFoundException, DocumentException, UbyInvalidArgumentException{
-		
+
 		UbyTestDbProvider testDbProvider = new UbyTestDbProvider();
 		this.uby = testDbProvider.getUby();
 
 	}
-	
-	
-
 
 	@Test
-	public void testLexicons() throws UbyInvalidArgumentException{		
+	public void testLexicons() throws UbyInvalidArgumentException{
 		List<String> lexiconNames = uby.getLexiconNames();
 		assertTrue(lexiconNames.contains("FrameNet"));
 		assertTrue(lexiconNames.contains("VerbNet"));
@@ -77,97 +74,97 @@ public class UbyTest
 		assertTrue(lexiconNames.contains("Wiktionary"));
 		assertTrue(lexiconNames.contains("Wikipedia"));
 		assertTrue(lexiconNames.contains("OmegaWikideu"));
-		
+
 		List<Lexicon> lexicons = uby.getLexicons();
 		assertEquals(lexiconNames.size(), lexicons.size());
-		
+
 		for(Lexicon lexicon : lexicons){
 			String name = lexicon.getName();
 			assertTrue(lexiconNames.contains(name));
 			assertEquals(uby.getLexiconByName(name).getId(), lexicon.getId());
 		}
-		
+
 		lexicons = uby.getLexiconsByLanguage(ELanguageIdentifier.ENGLISH);
 		assertEquals(lexicons.size(), 5);
 		assertEquals(lexicons.get(0).getLexicalEntries().get(0)
 			.getLemma().getFormRepresentations().get(0).getLanguageIdentifier(), ELanguageIdentifier.ENGLISH);
 	}
-	
+
 	@Test
 	public void testLexicalEntries() throws UbyInvalidArgumentException{
 		Lexicon lexicon = uby.getLexiconByName("FrameNet");
 		List<LexicalEntry> lexEntries = uby.getLexicalEntries("run", lexicon);
 		assertEquals(lexEntries.size(), 1);
-		
-		LexicalEntry lexEntry = lexEntries.get(0);		
-		lexEntry = uby.getLexicalEntryById(lexEntry.getId());		
-		lexEntry = uby.getLexicalEntries(lexEntry.getLemmaForm(), 
+
+		LexicalEntry lexEntry = lexEntries.get(0);
+		lexEntry = uby.getLexicalEntryById(lexEntry.getId());
+		lexEntry = uby.getLexicalEntries(lexEntry.getLemmaForm(),
 					   lexEntry.getPartOfSpeech(), lexicon).get(0);
-		
+
 		assertEquals(lexEntry.getId(), "FN_LexicalEntry_4417");
 		assertEquals(lexEntry.getPartOfSpeech(), EPartOfSpeech.verb);
-		
-		
+
+
 		List<Lexicon> lexicons = uby.getLexicons();
 		for(Lexicon l : lexicons){
 			Iterator<LexicalEntry> iter = uby.getLexicalEntryIterator(l);
-			int i = 0;			
+			int i = 0;
 			while(iter.hasNext()){
 				iter.next();
-				i++;				
+				i++;
 			}
 			assertEquals(i, l.getLexicalEntries().size());
 		}
-		
+
 		lexEntries = uby.getLexicalEntries("question", null);
-		
-		Set<String> lexNames = new HashSet<String>();		
+
+		Set<String> lexNames = new HashSet<String>();
 		for(LexicalEntry le : lexEntries){
 			assertEquals(le.getLemmaForm(), "question");
-			lexNames.add(le.getLexicon().getName());			
-		}		
-		assertTrue(lexNames.contains("VerbNet"));		
-		assertTrue(lexNames.contains("Wiktionary"));		
-		
+			lexNames.add(le.getLexicon().getName());
+		}
+		assertTrue(lexNames.contains("VerbNet"));
+		assertTrue(lexNames.contains("Wiktionary"));
+
 		lexEntries = uby.getLexicalEntriesByLemmaPrefix("ques", EPartOfSpeech.noun, null);
 		assertEquals(lexEntries.size(), 2);
 	}
-	
+
 	@Test
 	public void testSenses() throws UbyInvalidArgumentException{
-		
+
 		List<Lexicon> lexicons = uby.getLexicons();
 		for(Lexicon l : lexicons){
 			Iterator<Sense> iter = uby.getSenseIterator(l);
-			int i = 0;		
+			int i = 0;
 			String prefix = "";
 			while(iter.hasNext()){
 				Sense sense = iter.next();
 				if(prefix.isEmpty()){
 					prefix = sense.getId().split("_")[0];
 				}
-				i++;				
-			}			
+				i++;
+			}
 			int j = 0;
 			for(LexicalEntry le : l.getLexicalEntries()){
 				j += le.getSenses().size();
-			}			
+			}
 			assertEquals(i, j);
 			assertEquals(uby.getSensesbyIdPattern(prefix).size(), i);
 		}
-		
+
 		Sense sense = uby.getSenseById("VN_Sense_2");
 		assertEquals(sense.getIndex(), 2);
 		assertEquals(sense.getSenseExamples().get(0).getTextRepresentations()
 				.get(0).getWrittenText(), "I interrogated him as a suspect.");
 		assertEquals(sense.getSemanticLabels().get(0).getType(), ELabelTypeSemantics.verbnetClass);
-		
+
 		List<Sense> extSenses = uby.getSensesByOriginalReference("FrameNet 1.5 lexical unit ID", "10035");
 		assertEquals(extSenses.size(), 1);
 		for(Sense s : extSenses){
 			assertEquals(s.getLexicalEntry().getLexicon().getName(), "FrameNet");
 		}
-		
+
 		extSenses = uby.getSensesByOriginalReference("VerbNet v3.1", "question_interrogate-37.1.3");
 		assertEquals(extSenses.size(), 3);
 		int i = 0;
@@ -180,24 +177,33 @@ public class UbyTest
 		assertEquals(extSenses.size(), 1);
 		assertEquals(extSenses.get(0).getIndex(), 1);
 		assertEquals(extSenses.get(0).getLexicalEntry().getLexicon().getName(), "WordNet");
-		
+
 		extSenses = uby.getSensesByOriginalReference("Wiktionary sense key", "49890:1:2");
 		assertEquals(extSenses.get(0).getId(), "WktEn_sense_2");
 
 		extSenses = uby.getSensesByOriginalReference("OW SynTrans ID", "209021");
 		assertEquals(extSenses.get(0).getId(), "OW_deu_Sense_4143");
 
-		
-		
-		
+		Lexicon lexicon = new Lexicon();
+		lexicon.setName("WordNet");
+
+		extSenses = uby.getSensesByOriginalReference("WordNet 3.0 part of speech and sense key", "[POS: verb] question%2:32:03::", lexicon);
+        assertEquals(extSenses.size(), 1);
+        assertEquals(extSenses.get(0).getIndex(), 1);
+        assertEquals(extSenses.get(0).getLexicalEntry().getLexicon().getName(), "WordNet");
+
+        Lexicon lexicon1 = new Lexicon();
+        lexicon1.setName("FrameNet");
+
+        extSenses = uby.getSensesByOriginalReference("WordNet 3.0 part of speech and sense key", "[POS: verb] question%2:32:03::", lexicon1);
+        assertEquals(extSenses.size(), 0);
 	}
-	
-	
+
 	@Test
 	public void testSynsets() throws UbyInvalidArgumentException{
 		Lexicon lexicon = uby.getLexiconByName("WordNet");
 		Iterator<Synset> synsetIterator = uby.getSynsetIterator(lexicon);
-		
+
 		int i = 0;
 		while(synsetIterator.hasNext()){
 			Synset synset = synsetIterator.next();
@@ -208,7 +214,7 @@ public class UbyTest
 		}
 		assertEquals(i,6);
 	}
-	
+
 	@Test
 	public void testSenseAxes() throws UbyInvalidArgumentException{
 		List<SenseAxis> senseAxes = uby.getSenseAxes();
@@ -217,7 +223,7 @@ public class UbyTest
 		for(SenseAxis senseAxis : senseAxes){
 			senseAxisIds.add(senseAxis.getId());
 		}
-		
+
 		Iterator<SenseAxis> senseAxisIterator = uby.getSenseAxisIterator();
 		int i = 0;
 		while(senseAxisIterator.hasNext()){
@@ -233,18 +239,18 @@ public class UbyTest
 		senseAxes = uby.getSenseAxesByIdPattern("AnoEx_SenseAxis_7");
 		assertEquals(senseAxes.size(), 1);
 		assertEquals(senseAxes.get(0).getSenseOne().getId(), "WN_Sense_2");
-		
+
 		Sense sense = uby.getSenseById("WktEn_sense_3");
 		senseAxes = uby.getSenseAxesBySense(sense);
 		assertEquals(senseAxes.size(), 1);
 		assertEquals(senseAxes.get(0).getSenseTwo().getId(), "WN_Sense_7");
-		
+
 		Sense sense2 = uby.getSenseById("WN_Sense_7");
 		assertTrue(uby.hasSensesAxis(sense, sense2));
 		Sense sense3 = uby.getSenseById("VN_Sense_2");
 		assertFalse(uby.hasSensesAxis(sense, sense3));
 	}
-	
+
 	@Test
 	public void testSemanticPredicates() throws UbyInvalidArgumentException{
 		Lexicon lexicon = uby.getLexiconByName("FrameNet");
@@ -259,7 +265,7 @@ public class UbyTest
 		assertTrue(semanticPredicateIds.contains("FN_SemanticPredicate_29"));
 		assertTrue(semanticPredicateIds.contains("FN_SemanticPredicate_700"));
 		assertTrue(semanticPredicateIds.contains("FN_SemanticPredicate_591"));
-		
+
 		int i = 0;
 		while(semanticPredicateIterator.hasNext()){
 			SemanticPredicate semanticPredicate = semanticPredicateIterator.next();
@@ -267,7 +273,7 @@ public class UbyTest
 			i++;
 		}
 		assertEquals(i, 3);
-	
+
 		SemanticPredicate semanticPredicate = uby.getSemanticPredicateById("VN_SemanticPredicate_500");
 		List<SemanticArgument> semanticArguments = semanticPredicate.getSemanticArguments();
 		assertEquals(semanticArguments.size(),2);
@@ -275,18 +281,18 @@ public class UbyTest
 		for(SemanticArgument semanticArgument : semanticArguments){
 			SemanticArgument semanticArgument2 = uby.getSemanticArgumentById(semanticArgument.getId());
 			semanticRoles.add(semanticArgument2.getSemanticRole());
-		}		
+		}
 		assertTrue(semanticRoles.contains("Theme[+animate|+machine]"));
-		assertTrue(semanticRoles.contains("Location[+concrete]"));		
+		assertTrue(semanticRoles.contains("Location[+concrete]"));
 	}
-	
+
 	@Test
 	public void testSemanticLabels(){
 		List<SemanticLabel> semanticLabels = uby.getSemanticLabelsbySenseId("VN_Sense_2430");
 		assertEquals(semanticLabels.size(),1);
 		SemanticLabel semanticLabel = semanticLabels.get(0);
 		assertEquals(semanticLabel.getLabel(),"run-51.3.2");
-		
+
 		semanticLabels = uby.getSemanticLabelsbySenseIdbyType("WN_Sense_2", "semanticField");
 		assertEquals(semanticLabels.size(), 1);
 		semanticLabel = semanticLabels.get(0);
@@ -294,7 +300,7 @@ public class UbyTest
 		semanticLabels = uby.getSemanticLabelsbySenseIdbyType("WN_Sense_2", "verbnetClass");
 		assertEquals(semanticLabels.size(), 0);
 	}
-	
+
 	@Test
 	public void testSynSemArgMaps(){
 		List<SynSemArgMap> synSemArgMaps = uby.getSynSemArgMaps();
@@ -306,7 +312,7 @@ public class UbyTest
 			   synSemArgMap.getSemanticArgument().getId().equals("VN_SemanticArgument_7")){
 				found = true;
 				break;
-			}		   
+			}
 		}
 		assertTrue(found);
 	}
