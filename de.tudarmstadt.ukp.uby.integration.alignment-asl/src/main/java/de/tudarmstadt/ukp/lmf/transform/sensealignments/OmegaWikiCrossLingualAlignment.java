de1487e537c74c8dc96e852a866c0f3753300853
judithek <jek@eckle-kohler.de>
2014-10-17 07:47:13 +0000

    update issue 109. updated import class for internal crosslingual OW links to new (standardized) externalSystem value works now

--- de.tudarmstadt.ukp.uby.integration.alignment-asl/src/main/java/de/tudarmstadt/ukp/lmf/transform/sensealignments/OmegaWikiCrossLingualAlignment.java
+++ de.tudarmstadt.ukp.uby.integration.alignment-asl/src/main/java/de/tudarmstadt/ukp/lmf/transform/sensealignments/OmegaWikiCrossLingualAlignment.java
@@ -30,6 +30,7 @@ import java.util.Set;
 
 import de.tudarmstadt.ukp.lmf.model.core.Sense;
 import de.tudarmstadt.ukp.lmf.transform.alignments.SenseAlignment;
+import de.tudarmstadt.ukp.lmf.transform.omegawiki.OmegaWikiLMFMap;
 import de.tudarmstadt.ukp.omegawiki.api.OmegaWiki;
 import de.tudarmstadt.ukp.omegawiki.api.SynTrans;
 
@@ -39,6 +40,9 @@ public class OmegaWikiCrossLingualAlignment extends SenseAlignment
 	private final OmegaWiki ow;
 	private final int sourceLang;
 	private final int targetLang;
+	private final String sourceLangId;
+	private final String targetLangId;
+
 	private java.sql.Connection connection;
 	public OmegaWikiCrossLingualAlignment(String sourceUrl, String destUrl, String dbDriver, 
 			String dbVendor, String user, String pass, OmegaWiki ow, int sourceLang, int targetLang) throws ClassNotFoundException, SQLException, FileNotFoundException
@@ -47,6 +51,8 @@ public class OmegaWikiCrossLingualAlignment extends SenseAlignment
 		this.ow = ow;
 		this.sourceLang = sourceLang;
 		this.targetLang = targetLang;
+		this.sourceLangId = OmegaWikiLMFMap.mapLanguage(sourceLang);
+		this.targetLangId = OmegaWikiLMFMap.mapLanguage(targetLang);
 		logString = new StringBuilder();
 		Class.forName(dbDriver);
 		connection = DriverManager.getConnection("jdbc:"+dbVendor+"://"+sourceUrl,user,pass);
@@ -91,16 +97,20 @@ public class OmegaWikiCrossLingualAlignment extends SenseAlignment
 		System.out.println("Starting getting alignment for OmegaWiki ");
 		try {
 			int count = 0;
+		
 			Map <SynTrans,Set<SynTrans>> stm = ow.getInterlanguageSTLinks(sourceLang, targetLang);
 			System.out.println("Number of STlinks:" + stm.size());
+			System.out.println("source language: " + sourceLangId + " target language: " + targetLangId);
 			for(SynTrans source : stm.keySet()) {
 			//	List<Sense> first = ubySource.getSensesByOWSynTransId(""+source.getSyntransid());
-				List<Sense> first = ubySource.getSensesByOriginalReference("OW SynTrans ID", ""+source.getSyntransid());
+				// old external system value: "OW SynTrans ID"
+				// new external system value, e.g. for English: "OmegaWiki_eng_synTrans"
+				List<Sense> first = ubySource.getSensesByOriginalReference("OmegaWiki_"+sourceLangId+"_synTrans", ""+source.getSyntransid());
 				if (first.size()>0) {
 					Sense sourceSense = first.get(0);
 					System.out.print("First:" + sourceSense.getId());
 					for (SynTrans target : stm.get(source)){
-						List<Sense> second = ubyDest.getSensesByOriginalReference("OW SynTrans ID", ""+target.getSyntransid());
+						List<Sense> second = ubyDest.getSensesByOriginalReference("OmegaWiki_"+targetLangId+"_synTrans", ""+target.getSyntransid());
 						if (second.size() > 0) {
 							Sense targetSense = second.get(0);
 							System.out.print("Second:" + targetSense.getId());
