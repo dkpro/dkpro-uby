fd32f125064665b6f0e670392fab41fd90527805
eckle.kohler@gmail.com <eckle.kohler@gmail.com@96d683f9-5a14-a1d8-75b7-d247282e0859>
2014-10-20 10:44:28 +0000

    updates issue 109: update OW to new externalSystem value updates issue 115: import new alignments based on Dijkstra-WSA - removed method getAlignmentDirectSQL()

--- de.tudarmstadt.ukp.uby/trunk/de.tudarmstadt.ukp.uby.integration.alignment-asl/src/main/java/de/tudarmstadt/ukp/lmf/transform/sensealignments/OmegaWikiWiktionaryAlignment.java
+++ de.tudarmstadt.ukp.uby/trunk/de.tudarmstadt.ukp.uby.integration.alignment-asl/src/main/java/de/tudarmstadt/ukp/lmf/transform/sensealignments/OmegaWikiWiktionaryAlignment.java
@@ -21,13 +21,7 @@ package de.tudarmstadt.ukp.lmf.transform.sensealignments;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.UnsupportedEncodingException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
@@ -35,31 +29,31 @@ import de.tudarmstadt.ukp.lmf.model.core.Sense;
 import de.tudarmstadt.ukp.lmf.transform.DBConfig;
 import de.tudarmstadt.ukp.lmf.transform.alignments.SenseAlignment;
 import de.tudarmstadt.ukp.lmf.transform.alignments.SenseAlignmentUtils;
+import de.tudarmstadt.ukp.lmf.transform.omegawiki.OmegaWikiLMFMap;
 import de.tudarmstadt.ukp.omegawiki.api.DefinedMeaning;
 import de.tudarmstadt.ukp.omegawiki.api.OmegaWiki;
 import de.tudarmstadt.ukp.omegawiki.api.SynTrans;
-import de.tudarmstadt.ukp.omegawiki.exception.OmegaWikiException;
 
 public class OmegaWikiWiktionaryAlignment
 	extends SenseAlignment
 {
 	public StringBuilder logString;
 	private final SenseAlignmentUtils saUtils;
-	private final Connection alignmentConnection;
 	private final OmegaWiki ow;
 	private final int owLanguage;
+	private final String owLangId;
+
 	
 	public OmegaWikiWiktionaryAlignment(String sourceUrl, String destUrl,String dbDriver,
 			String dbVendor, String alignmentFile, String user, String pass,
-			String alignmentHost,String alignmentDb,String alignmentUser, 
-			String alignmentPass,OmegaWiki ow,int language) throws SQLException, InstantiationException, 
+			OmegaWiki ow,int language) throws SQLException, InstantiationException, 
 			IllegalAccessException, ClassNotFoundException, FileNotFoundException
 	{
 		super(sourceUrl, destUrl,dbDriver,dbVendor, alignmentFile, user, pass,UBY_HOME);
 		this.ow = ow;
 		this.owLanguage = language;
+		this.owLangId = OmegaWikiLMFMap.mapLanguage(owLanguage);
 		Class.forName(dbDriver);
-		alignmentConnection=  DriverManager.getConnection("jdbc:"+dbVendor+"://"+alignmentHost+"/"+alignmentDb, alignmentUser, alignmentPass);
 		DBConfig s = new DBConfig(sourceUrl,dbDriver,dbVendor, user, pass, true);
 		DBConfig d = new DBConfig(destUrl,dbDriver,dbVendor,user, pass, true);
 
@@ -69,48 +63,6 @@ public class OmegaWikiWiktionaryAlignment
 		logString = new StringBuilder();
 	}
 
-	public void getAlignmentDirectSQL() throws SQLException, OmegaWikiException, UnsupportedEncodingException, ClassNotFoundException
-	{
-		int count =0;
-		HashMap<String,String> ubyMap = new HashMap<String, String>();
-		HashMap<String,String> owMap = new HashMap<String, String>();
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:mysql://uby.ukp.informatik.tu-darmstadt.de/uby_release_1_0","matuschek","p?h;fkyt");
-		Statement statement = conn.createStatement();
-		ResultSet resultSet = statement.executeQuery("SELECT Sense.senseId, MonolingualExternalRef.externalReference  FROM Sense JOIN MonolingualExternalRef ON (Sense.senseId=MonolingualExternalRef.senseId) where Sense.senseId like 'WktE%'or Sense.senseId like 'OW_en%' ");
-		while (resultSet.next())
-		{
-			ubyMap.put(resultSet.getString(2), resultSet.getString(1));
-
-		}
-		resultSet = statement.executeQuery("SELECT _index,  senseId, synsetId FROM Sense where senseId like 'OW_%' ");
-		while (resultSet.next())
-		{
-			owMap.put(resultSet.getString(1), resultSet.getString(2)+"#"+resultSet.getString(3));
-		}
-		statement = alignmentConnection.createStatement();
-		resultSet = statement.executeQuery("select ID1,ID2 from WKTOWRelatedness_classify where ClassTraining = '1'");
-		while (resultSet.next()){
-			DefinedMeaning dm = ow.getDefinedMeaningById(resultSet.getInt("ID2")); //Synset!!!
-			Set<SynTrans> sts = dm.getSynTranses(owLanguage); //Senses!!!
-			String[] temp = resultSet.getString("ID1").split("--");
-			String wktId = ubyMap.get(temp[1]);
-			statement = conn.createStatement();
-			for (SynTrans st : sts)
-			{
-				String sourceSense = owMap.get(st.getSyntransid()+"");
-				if(wktId == null) {
-					continue;
-				}
-				String[] temp2 = sourceSense.split("#");
-				String command = "insert into SenseAxis(senseAxisId, senseAxisType, senseOneID, senseTwoId,synsetOneId,synsetTwoId,lexicalResourceId,idx) Values("+
-								"'Wiktionary_OmegaWiki_Matuschek_2012_"+count+++"','monolingualSenseAlignment','"+temp2[0]+"','"+wktId+"','"+temp2[1]+"',NULL,'Uby',NULL);";
-				statement.executeUpdate(command);
-			}
-
-		}
-
-	}
 	@Override
 	public void getAlignment()
 	{
@@ -140,7 +92,7 @@ public class OmegaWikiWiktionaryAlignment
 
 				for (SynTrans st : sts)
 				{
-					List<Sense> first = ubySource.getSensesByOriginalReference("OW SynTrans ID", ""+st.getSyntransid());
+					List<Sense> first = ubySource.getSensesByOriginalReference("OmegaWiki_"+owLangId+"_synTrans", ""+st.getSyntransid());
 					Sense sourceSense = first.get(0);
 					for (Sense targetSense: senseWKN)
 					{
