a131dc9592ec700c18581a19c06ab0cc3d2b7b0c
matu011235@googlemail.com <matu011235@googlemail.com@96d683f9-5a14-a1d8-75b7-d247282e0859>
2014-11-19 14:42:00 +0000

    C program for calculating shortest paths is no longer needed - removing the corresponding files

--- de.tudarmstadt.ukp.uby/trunk/de.tudarmstadt.ukp.uby.integration.alignment-asl/src/main/resources/dijkstra_wsa.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-./pairwiseShortestPaths.exe ../../../target/WN_synset_Pos_relationMLgraph_1000.txt_MERGED_OW_en_synset_Pos_relationMLgraph_500_trivial.txt ../../../target/WN_OW_en_candidates_Pos ../../../target/WN_synset_Pos_relationMLgraph_1000.txt_MERGED_OW_en_synset_Pos_relationMLgraph_500_trivial_result.txt
-
-
