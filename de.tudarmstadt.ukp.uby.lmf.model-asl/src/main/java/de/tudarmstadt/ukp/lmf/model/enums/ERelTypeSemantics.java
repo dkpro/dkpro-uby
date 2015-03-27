bf9a74107fddd506fe8abead1b77356df5c34ae0
judithek <jek@eckle-kohler.de>
2015-03-27 15:02:12 +0000

    updates issue 32. FrameNet integration: adapt PredicateRelation relType and relName to terminology used in ERelTypeSemantics and ERelNameSemantics - added new values to ERelTypeSemantics - did not harmonize the actual values however, since they are FrameNet specific and might be required by users familiar with FrameNet

--- de.tudarmstadt.ukp.uby.lmf.model-asl/src/main/java/de/tudarmstadt/ukp/lmf/model/enums/ERelTypeSemantics.java
+++ de.tudarmstadt.ukp.uby.lmf.model-asl/src/main/java/de/tudarmstadt/ukp/lmf/model/enums/ERelTypeSemantics.java
@@ -2,13 +2,13 @@
  * Copyright 2015
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,23 +17,29 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.lmf.model.enums;
 
+import de.tudarmstadt.ukp.lmf.model.semantics.SenseRelation;
+import de.tudarmstadt.ukp.lmf.model.semantics.SynsetRelation;
+
 /**
  * Enumeration of possible relation types specified by an instance of
  * {@link SenseRelation} or {@link SynsetRelation} class.
  */
 public enum ERelTypeSemantics {
-	
+
 	taxonomic, // e.g. hyponym
 	partWhole, // e.g. metonym
 	association, // e.g. see also
-	label,	// e.g. region, topc, but also capital, currency 
-	predicative, // e.g. is played by, flows through 
+	label,	// e.g. region, topc, but also capital, currency
+	predicative, // e.g. is played by, flows through
 	complementary, // e.g. antonym
-	
+	frameRelation, // e.g. subframe, relation between semantic predicates
+	sameFrame, // e.g. excludes, relation between semantic predicates
+	coreSet, // e.g. core_set_4 relation between semantic arguments
+
 	@Deprecated translation,
 
 	@Deprecated labelOmegaWiki,
-	
+
 	@Deprecated predicativeOmegaWiki
-	
+
 }
