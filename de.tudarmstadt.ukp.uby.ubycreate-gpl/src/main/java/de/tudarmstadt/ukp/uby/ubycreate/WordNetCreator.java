d8ce567291a25f17ace20cd7a926e2dacdb7c867
Tristan Miller <psychonaut@nothingisreal.com>
2016-06-10 15:00:25 +0200

    Issue #170. Upgrade hard-coded extJWNL properties file.

--- de.tudarmstadt.ukp.uby.ubycreate-gpl/src/main/java/de/tudarmstadt/ukp/uby/ubycreate/WordNetCreator.java
+++ de.tudarmstadt.ukp.uby.ubycreate-gpl/src/main/java/de/tudarmstadt/ukp/uby/ubycreate/WordNetCreator.java
@@ -25,13 +25,14 @@ public class WordNetCreator
     private static final String dtdVersion = "1_0";
 
     /**
-     * This method converts wordNet into ubyXML,stores it into a temporary file and then migrates
-     * the data from the temporary file into DB
+     * This method converts wordNet into ubyXML,stores it into a temporary file
+     * and then migrates the data from the temporary file into DB
      *
      */
     @Override
     public void lexicon2DB(final DBConfig dbConfig, String source)
-        throws IOException, XMLStreamException, SAXException, DocumentException, JWNLException
+        throws IOException, XMLStreamException, SAXException,
+        DocumentException, JWNLException
     {
 
         String lexicalName = "WordNet";
@@ -51,7 +52,8 @@ public class WordNetCreator
 
     @Override
     public File lexicon2XML(String source, File lmfXML)
-        throws IOException, XMLStreamException, SAXException, DocumentException, JWNLException
+        throws IOException, XMLStreamException, SAXException,
+        DocumentException, JWNLException
     {
 
         String lexicalResourceName = "WordNet_3.0_eng";
@@ -63,59 +65,67 @@ public class WordNetCreator
         File wnPath = new File(source);
         Dictionary extWordnet;
         extWordnet = Dictionary
-                .getInstance(IOUtils.toInputStream(
-                        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+                .getInstance(IOUtils
+                        .toInputStream("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
                                 + "<jwnl_properties language=\"en\">"
-                                + "  <version publisher=\"Princeton\" number=\"3.0\" language=\"en\"/>"
-                                + "  <dictionary class=\"net.sf.extjwnl.dictionary.FileBackedDictionary\">"
-                                + "    <param name=\"morphological_processor\" value=\"net.sf.extjwnl.dictionary.morph.DefaultMorphologicalProcessor\">"
-                                + "      <param name=\"operations\">"
-                                + "        <param value=\"net.sf.extjwnl.dictionary.morph.LookupExceptionsOperation\"/>"
-                                + "        <param value=\"net.sf.extjwnl.dictionary.morph.DetachSuffixesOperation\">"
-                                + "          <param name=\"noun\" value=\"|s=|ses=s|xes=x|zes=z|ches=ch|shes=sh|men=man|ies=y|\"/>"
-                                + "          <param name=\"verb\" value=\"|s=|ies=y|es=e|es=|ed=e|ed=|ing=e|ing=|\"/>"
-                                + "          <param name=\"adjective\" value=\"|er=|est=|er=e|est=e|\"/>"
-                                + "          <param name=\"operations\">"
-                                + "            <param value=\"net.sf.extjwnl.dictionary.morph.LookupIndexWordOperation\"/>"
-                                + "            <param value=\"net.sf.extjwnl.dictionary.morph.LookupExceptionsOperation\"/>"
-                                + "          </param>"
-                                + "        </param>"
-                                + "        <param value=\"net.sf.extjwnl.dictionary.morph.TokenizerOperation\">"
-                                + "          <param name=\"delimiters\">"
-                                + "            <param value=\" \"/>"
-                                + "            <param value=\"-\"/>"
-                                + "          </param>"
-                                + "          <param name=\"token_operations\">"
-                                + "            <param value=\"net.sf.extjwnl.dictionary.morph.LookupIndexWordOperation\"/>"
-                                + "            <param value=\"net.sf.extjwnl.dictionary.morph.LookupExceptionsOperation\"/>"
-                                + "            <param value=\"net.sf.extjwnl.dictionary.morph.DetachSuffixesOperation\">"
-                                + "              <param name=\"noun\" value=\"|s=|ses=s|xes=x|zes=z|ches=ch|shes=sh|men=man|ies=y|\"/>"
-                                + "              <param name=\"verb\" value=\"|s=|ies=y|es=e|es=|ed=e|ed=|ing=e|ing=|\"/>"
-                                + "              <param name=\"adjective\" value=\"|er=|est=|er=e|est=e|\"/>"
-                                + "              <param name=\"operations\">"
-                                + "                <param value=\"net.sf.extjwnl.dictionary.morph.LookupIndexWordOperation\"/>"
+                                + "    <version publisher=\"Princeton\" number=\"3.0\" language=\"en\"/>"
+                                + "    <dictionary class=\"net.sf.extjwnl.dictionary.FileBackedDictionary\">"
+                                + "        <param name=\"morphological_processor\" value=\"net.sf.extjwnl.dictionary.morph.DefaultMorphologicalProcessor\">"
+                                + "            <param name=\"operations\">"
                                 + "                <param value=\"net.sf.extjwnl.dictionary.morph.LookupExceptionsOperation\"/>"
-                                + "              </param>"
+                                + "                <param value=\"net.sf.extjwnl.dictionary.morph.DetachSuffixesOperation\">"
+                                + "                    <param name=\"noun\" value=\"|s=|ses=s|xes=x|zes=z|ches=ch|shes=sh|men=man|ies=y|\"/>"
+                                + "                    <param name=\"verb\" value=\"|s=|ies=y|es=e|es=|ed=e|ed=|ing=e|ing=|\"/>"
+                                + "                    <param name=\"adjective\" value=\"|er=|est=|er=e|est=e|\"/>"
+                                + "                    <param name=\"operations\">"
+                                + "                        <param value=\"net.sf.extjwnl.dictionary.morph.LookupIndexWordOperation\"/>"
+                                + "                        <param value=\"net.sf.extjwnl.dictionary.morph.LookupExceptionsOperation\"/>"
+                                + "                    </param>"
+                                + "                </param>"
+                                + "                <param value=\"net.sf.extjwnl.dictionary.morph.TokenizerOperation\">"
+                                + "                    <param name=\"delimiters\">"
+                                + "                        <param value=\" \"/>"
+                                + "                        <param value=\"-\"/>"
+                                + "                    </param>"
+                                + "                    <param name=\"token_operations\">"
+                                + "                        <param value=\"net.sf.extjwnl.dictionary.morph.LookupIndexWordOperation\"/>"
+                                + "                        <param value=\"net.sf.extjwnl.dictionary.morph.LookupExceptionsOperation\"/>"
+                                + "                        <param value=\"net.sf.extjwnl.dictionary.morph.DetachSuffixesOperation\">"
+                                + "                            <param name=\"noun\" value=\"|s=|ses=s|xes=x|zes=z|ches=ch|shes=sh|men=man|ies=y|\"/>"
+                                + "                            <param name=\"verb\" value=\"|s=|ies=y|es=e|es=|ed=e|ed=|ing=e|ing=|\"/>"
+                                + "                            <param name=\"adjective\" value=\"|er=|est=|er=e|est=e|\"/>"
+                                + "                            <param name=\"operations\">"
+                                + "                                <param value=\"net.sf.extjwnl.dictionary.morph.LookupIndexWordOperation\"/>"
+                                + "                                <param value=\"net.sf.extjwnl.dictionary.morph.LookupExceptionsOperation\"/>"
+                                + "                            </param>"
+                                + "                        </param>"
+                                + "                    </param>"
+                                + "                </param>"
                                 + "            </param>"
-                                + "          </param>"
                                 + "        </param>"
-                                + "      </param>"
-                                + "    </param>"
-                                + "    <param name=\"dictionary_element_factory\" value=\"net.sf.extjwnl.princeton.data.PrincetonWN17FileDictionaryElementFactory\"/>"
-                                + "    <param name=\"file_manager\" value=\"net.sf.extjwnl.dictionary.file_manager.FileManagerImpl\">"
-                                + "      <param name=\"file_type\" value=\"net.sf.extjwnl.princeton.file.PrincetonRandomAccessDictionaryFile\"/>"
-                                + "      <param name=\"dictionary_path\" value=\""
-                                + wnPath.getAbsolutePath() + "\"/>"
-                                + "    </param>"
-                                + "  </dictionary>"
+                                + "        <param name=\"dictionary_element_factory\""
+                                + "               value=\"net.sf.extjwnl.princeton.data.PrincetonWN17FileDictionaryElementFactory\"/>"
+                                + "        <param name=\"file_manager\" value=\"net.sf.extjwnl.dictionary.file_manager.FileManagerImpl\">"
+                                + "            <param name=\"file_type\" value=\"net.sf.extjwnl.princeton.file.PrincetonRandomAccessDictionaryFile\">"
+                                + "                <!--<param name=\"write_princeton_header\" value=\"true\"/>-->"
+                                + "                <!--<param name=\"encoding\" value=\"UTF-8\"/>-->"
+                                + "            </param>"
+                                + "            <!--<param name=\"cache_use_count\" value=\"true\"/>-->"
+                                + "            <param name=\"dictionary_path\" value=\""
+                                + wnPath.getAbsolutePath()
+                                + "\"/>"
+                                + "        </param>"
+                                + "    </dictionary>"
+                                + "    <resource class=\"net.sf.extjwnl.princeton.PrincetonResource\"/>"
                                 + "</jwnl_properties>"));
 
-        WNConverter converterWN = new WNConverter(wnPath, extWordnet, new LexicalResource(),
-                lexicalResourceName, dtdVersion);
+        WNConverter converterWN = new WNConverter(wnPath, extWordnet,
+                new LexicalResource(), lexicalResourceName, dtdVersion);
         converterWN.toLMF();
         lexicalResource = converterWN.getLexicalResource();
 
-        LMFXmlWriter xmlWriter = new LMFXmlWriter(lmfXML.getAbsolutePath(), dtdPath);
+        LMFXmlWriter xmlWriter = new LMFXmlWriter(lmfXML.getAbsolutePath(),
+                dtdPath);
         xmlWriter.writeElement(lexicalResource);
         xmlWriter.writeEndDocument();
 
